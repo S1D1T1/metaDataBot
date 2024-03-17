e0cb6cc4d6855ee73d94dba8216fdb303073ed08
S1D1T1 <156350598+S1D1T1@users.noreply.github.com>
2024-03-17 12:33:19 -0400

    Add files via upload

---
+++ source/image processing.swift
@@ -0,0 +1,182 @@
+//
+//  image processing.swift
+//  discordbot
+//
+//  Created by sodoto on 3/16/24.
+//
+// MIT License
+
+import Foundation
+import Discord
+import AppKit //  Core graphics
+
+// MARK: Image Metadata parsing
+
+func handleImage(_ u:URL, _ channel:Messageable){
+  if let params =   getEmbeddedParamString(u) {
+    if let dictionary = JsonStringtoDict(params) {
+      // here we go. translations
+      let translatedParams = translateParams(dictionary)
+      do {
+        let jsonData = try JSONSerialization.data(withJSONObject: translatedParams, options: [.prettyPrinted,.sortedKeys])
+        if let jsonString = String(data: jsonData, encoding: .utf8) {
+          log("posting \(translatedParams.count) params as json")
+          trackParameterHighScore(translatedParams.count, channel:channel)
+          sendAsFile("\(jsonString)",filename:"metadata.js", channel) //  send as a js file since this is actual valid json
+        }
+      }
+      catch{}
+    }
+    else {
+      // announce the prompts
+      announceDescriptionString(params,channel)
+    }
+  }
+}
+
+func translateParams(_ dictionary:[String:Any]) -> [String:Any] {
+  var returnVal = [String:Any]()
+
+  let paramNameTranslation:[String:String] = ["c":"prompt","uc":"negative_prompt"]
+
+  for (key, value) in dictionary {
+    if let newKey = paramNameTranslation[key] {
+      returnVal[newKey] = value
+    } else {
+      returnVal[key] = value
+    }
+  }
+  return returnVal
+}
+
+func getEmbeddedParamString(_ url: URL) -> String? {
+  guard let source = CGImageSourceCreateWithURL(url as CFURL, nil) else {
+    print("Error: Could not create image source from url")
+    return nil
+  }
+  let properties = CGImageSourceCopyPropertiesAtIndex(source, 0,nil) as? [String: Any]
+
+  // try to get exif 1st
+
+  if let exifProperties = properties?[kCGImagePropertyExifDictionary as String] as? [String: Any],
+     let jsonString = exifProperties["UserComment"] as? String {
+
+    return jsonString
+  }
+  else {
+    guard let pngProperties = properties?[kCGImagePropertyTIFFDictionary as String] as? [String: Any] else {
+      print("Error: Could not get PNG properties")
+      return nil
+    }
+    return pngProperties["ImageDescription"] as? String
+  }
+}
+
+func JsonStringtoDict(_ string:String) -> [String:Any]?{
+  do{
+    if let dictionary = try JSONSerialization.jsonObject(with: string.data(using: .utf8)!,
+                                                         options: []) as? [String: Any] {
+      return dictionary
+    }
+  }
+  catch{}
+  return nil
+}
+
+func prettyPrintParams(_ rawText: String) -> String {
+    var isInParens = false
+    var outString = ""
+
+    var chars = rawText.makeIterator()
+    while let char = chars.next() {
+        if char == ","  && !isInParens {
+          outString.append("\n")
+        } else if char == "(" {
+            isInParens = true
+          outString.append(char)
+
+        } else if char == ")" {
+            isInParens = false
+          outString.append(char)
+        } else {
+          outString.append(char)
+        }
+    }
+    return outString
+}
+
+/// extract prompt, neg prompt, & params. straightup json. NO translation, or filtering
+func announceDescriptionString(_ desc:String, _ channel:Messageable) {
+  let promptOnly = exifbotState.botState == .promptOnlyMode
+
+  let negPrefix = "\n-"
+  var textBlocks:[String.SubSequence]
+  let posPrompt:String
+  let negPrompt:String
+  var params:String
+
+  var sayString = "I found image data:"
+  if promptOnly { sayString.append(" (showing prompts only)")}
+  say(sayString,channel)
+  sayString = ""
+
+  if desc.contains(negPrefix) {
+    textBlocks = desc.split(separator: negPrefix)
+
+    if textBlocks.count < 2 {
+      return
+    }
+
+    posPrompt = String(textBlocks[0]).trimmingCharacters(in: .whitespacesAndNewlines)
+    textBlocks = textBlocks[1].split(separator: "\n")
+
+    let lineCount = textBlocks.count
+    if lineCount > 2 {
+      negPrompt = textBlocks[0...textBlocks.count-2].joined().trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    else {
+      negPrompt = String(textBlocks[0]).trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+  }
+  else {  //  no neg prompt. treat it as posPrompt ...\r params - could be multiple newlines
+    textBlocks = desc.split(separator: "\n")
+    let lineCount = textBlocks.count
+    if lineCount > 2 {
+      posPrompt = textBlocks[0...textBlocks.count-2].joined().trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    else {
+      posPrompt = String(textBlocks[0]).trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+
+    negPrompt = ""
+  }
+  sayString.append("Prompt: \(posPrompt)\n")
+  sayString.append("Negative Prompt: \(negPrompt.isEmpty ? "<none>" : negPrompt)\n ")
+
+  if !promptOnly {
+    params = String(textBlocks.last!)
+    let formattedParams = prettyPrintParams(params)
+
+    sayString.append("\(formattedParams)")
+    var paramcount = formattedParams.split(separator: "\n").count
+    log("posting text block of \(paramcount) params (not counting prompts)")
+
+    if !posPrompt.isEmpty {paramcount += 1 }
+    if !negPrompt.isEmpty {paramcount += 1 }
+    trackParameterHighScore(paramcount, channel:channel)
+  }
+  sendAsFile(sayString, channel)
+
+  return
+}
+
+/// Gamify. Create Engagement.
+func trackParameterHighScore(_ score:Int, channel: Messageable) {
+  if score > exifbotState.maxParameters {
+    exifbotState.maxParameters = score
+    log("New Parameter High Score!: \(score)")
+    if score > 20 {
+      say ("🥳 New Parameter High Score! 🥳: \(score) parameters",channel)
+    }
+  }
+}
---
+++ source/main.swift
@@ -0,0 +1,282 @@
+//
+//  main.swift
+//  discordbot
+//
+// copyright 2024 (c) SoDoTo
+// MIT License
+
+import Foundation
+import Discord
+import SwiftUI  // for @AppStorage
+import AppKit
+
+// MARK: setup
+
+class ExifbotState {
+  var bot:Bot
+  var saveLogString = ""  // use new sys logging feature
+  var botState:responseMode = .normalMode
+  var lastURL = ""
+
+  // stats
+  @AppStorage("imageCount") var imageCount = 0
+  @AppStorage("maxParameters") var maxParameters = 0
+  @AppStorage("startDate") var startDate = Date().description
+
+  init() {
+    let  myIntents:Set<Intents> = [.guildMessages,.messageContent,.dmMessages]
+    self.bot = Bot(token: "token removed", intents: myIntents)
+    self.bot.once {
+      let presenceString = "Pics, posting params"
+      self.bot.updatePresence(status:.online , activity:.watching(presenceString))
+    }
+  }
+}
+
+enum responseMode : Int { case normalMode=0, muteMode, promptOnlyMode }
+
+var exifbotState = ExifbotState()
+
+// MARK: Info Strings
+
+let helpString =
+"""
+## exifbot commands:
+
+Precede commands with "exifbot ", eg: "exifbot lastimage"
+
+  **lastimage** OR @ParamPeek: display the metadata for the most recent image posted in the current channel
+### Modes:
+  **mute**: report image metadata only when requested, via 'lastimage'
+  **unmute**: post metadata after all images (default)
+  **brief**: display only prompt & negative prompt
+  **full**: post metadata after all images (default)
+### Other
+  **wrong**: report incorrect or badly formatted image data
+  **version**: version info
+  **about**: about exifbot
+  **help**: show this message
+
+You can also DM Me an image to see its metadata
+"""
+
+let aboutString =
+"""
+Credits:
+exifbot version 0.1
+MIT License
+copyright(c) 2024 @sodot0
+
+Built using Discord Swift library by @Defxult. MIT License
+<https://github.com/Defxult/Discord.swift>
+"""
+let errorString =
+"""
+Thanks for the feedback. We'll take a look.
+Feel free to DM me any details about what was wrong
+"""
+
+
+runbot()  //  this never returns
+
+//-----------------------------
+
+func runbot() {
+  log("Starting up")
+
+  let bot = exifbotState.bot
+
+  try! bot.addListeners(ExifbotListener(name: "exifbot"))
+
+
+  bot.addSlashCommand(
+      name: "exifcommand",
+      description: "Example command",
+      guildId: nil,
+      onInteraction: { interaction in
+          try! await interaction.respondWithMessage("sample slash command", ephemeral: true)
+      }
+  )
+
+//  Task{
+//    try! await bot.syncApplicationCommands() // Only needs to be done once
+//  }
+
+  // updateBotActivity(state: .normalMode,bot:bot)  //  crash
+  bot.run()
+}
+
+class ExifbotListener : EventListener {
+  var presenceString = ""
+
+  func setExifMode(_ response:String, _ mode:responseMode, _ message: Message) {
+    say (response,message.channel)
+    exifbotState.botState = mode
+
+    // display current activity level
+
+    switch exifbotState.botState {
+      case .muteMode:
+        presenceString = "None"
+        log("setting mode: mute",message.channel)
+
+      case .normalMode:
+        presenceString = "Pics, posting params"
+        log("setting mode: unmute",message.channel)
+      case .promptOnlyMode:
+        presenceString = "Pics, posting only prompts"
+        log("setting mode: prompts only",message.channel)
+    }
+    message.channel.bot?.updatePresence(status:.online , activity:.watching(presenceString))
+  }
+
+  func onReady(me: ClientUser){
+    print("called onready")// this never gets called
+  }
+
+  func lastImage(_ message:Message) async{
+    do {
+      let pastMessages = try await message.channel.history()
+      print(pastMessages.count)
+
+      for aMessage in pastMessages {
+        if !aMessage.attachments.isEmpty,
+           let theType = aMessage.attachments[0].contentType,
+           theType.starts(with: "image/") {
+          // post a link to the message we found
+          say(aMessage.jumpUrl,message.channel)
+          processImage(aMessage.attachments[0],message)
+          return
+        }
+      }
+    }
+    catch {}
+  }
+
+
+  func processImage(_ attachment: Message.Attachment, _ message: Message) {
+
+    if let u = URL(string: attachment.url) {
+      log(attachment.url)
+      exifbotState.lastURL = attachment.url
+      handleImage(u,message.channel)
+      return
+    }
+  }
+
+  override func onMessageCreate(message: Message) async {
+    // Don't respond to our own message
+    guard !message.author.isBot else {
+      return
+    }
+    // MARK: Bot Commands
+
+    if message.isMentioned || message.content.lowercased() == "exifbot lastimage" {
+      say("Getting metadata for most recent image in this channel",message.channel)
+      await lastImage(message)
+    }
+
+    else if message.content == "hi exifbot" {
+      say("Hello",message.channel)
+    }
+    else if message.content.lowercased() == "exifbot version" {
+      say("Exifbot version 0.1",message.channel)
+    }
+
+    else if message.content.lowercased() == "exifbot mute" {
+      setExifMode("muting",.muteMode,message)
+    }
+
+    else if message.content.lowercased() == "exifbot unmute" {
+      setExifMode("unmuting",.normalMode,message)
+    }
+    else if message.content.lowercased() == "exifbot brief" {
+      setExifMode("prompts only",.promptOnlyMode,message)
+    }
+
+    else if message.content.lowercased() == "exifbot full" {
+      setExifMode("full metadata",.normalMode,message)
+    }
+
+    else if message.content.lowercased() == "exifbot about" {
+      aboutExifBot(message.channel)
+    }
+
+    else if message.content.lowercased() == "exifbot wrong" {
+      log("User Reports error", message.channel)
+      say ("Problem reported processing the previous image, at URL:<\(exifbotState.lastURL)>",message.channel)
+      say (errorString,message.channel)
+    }
+
+    else if message.content.lowercased() == "exifbot help" {
+      say (helpString,message.channel)
+    }
+
+    // MARK: Image Attachments
+    else if !message.attachments.isEmpty,
+            exifbotState.botState != .muteMode {
+      for attachment in message.attachments {
+        if let theType = attachment.contentType,
+           theType.starts(with: "image/") {
+
+
+          if let user = message.author.displayName {
+            say("### thanks for posting an image \(user)",message.channel)
+            log("image posted by \(user)",message.channel)
+            exifbotState.imageCount += 1
+            log("Image Count: \(exifbotState.imageCount)")
+          }
+
+
+          processImage(attachment, message)
+        }
+      }
+    }
+  }
+}
+
+// MARK: Sending messages & files
+
+func say(_ s:String,_ channel:Messageable)  {
+  Task {
+    do{
+      try await channel.send(s)
+    }
+    catch{}
+  }
+}
+
+func sendAsFile(_ s:String,filename:String = "metadata.txt",_ channel:Messageable){
+  Task {
+    do{
+      if !s.isEmpty,
+         let data = s.data(using: .utf8) {
+        let fileEmbed = File(name: filename, using: data)
+        try await channel.send(files:[fileEmbed])
+      }
+    }
+    catch{}
+  }
+}
+
+func aboutExifBot(_ channel:Messageable){
+  var sayString = aboutString
+  sayString.append("\n\(exifbotState.imageCount) Images processed, Since starting on \(exifbotState.startDate)\n")
+  sayString.append("Parameter High Score: \(exifbotState.maxParameters) parameters")
+  say(sayString,channel)
+}
+
+
+func log(_ s:String, _ channel:Messageable? = nil){
+
+  let now = Date().description.replacingOccurrences(of: " +0000", with: "")
+  var logString = "ExifBot - \(now):\(s)\n"
+  if let channel {
+    logString.append("channel ID: \(channel.id)")
+  }
+  print(logString)
+  exifbotState.saveLogString.append("\(logString)\n")
+}
+
+
+ 
